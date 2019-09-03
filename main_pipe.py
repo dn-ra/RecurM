3984aebf8294a8aaebc87c6e47c9da89dfc104d5
DanR <rawlinson.dp@gmail.com>
2019-09-03 12:10:25 +1000

    including fragment parsing

--- main_pipe.py
+++ main_pipe.py
@@ -24,7 +24,9 @@ cluster_method = 'union-find' #set to union-find or single-linkage
 
 #set empty vector of all nucmer_matches that pass threshold (=0.90 by default)
 collated_sig_matches = []
-
+sigmatch_set = set()
+#set empty vector to collect all nucmer_matches that are fragments of larger assemblies
+fragments = []
 
 #read in nucmer matches of each deltafile
 for file in os.listdir(delta_dir):
@@ -35,32 +37,52 @@ for file in os.listdir(delta_dir):
         
         #threshold all matches and collate
         print('thresholding {}'.format(file))
-        collated_sig_matches += delta_parse.dict_threshold(delta, threshold = 0.90, collate = True)
-        #exit type is list of Nucmer_Match objects
+        firstpass_fragments = []
+        for m in next(iter(delta.values())):
+            stats = m.gen_statistics()
+            if m.apply_threshold(treshold = 0.90, stats = stats) == True:
+                collated_sig_matches.append(m)
+                sigmatch_set.add([m.seqs])
+            elif m.is_fragment(upperthreshold = 0.90, lowerthreshold = 0.90, stats = stats):
+                firstpass_fragments.append(m)
+    
+        #all match objects are read in. keep only those fragments that map to full matches    
+        for m in firstpass_fragments;:
+            if m.seqs[0] in sigmatch_set:
+                fragments.append(m)
+            elif m.seqs[1] in sigmatch_set:
+                fragments.append(m)
+                    
+        #exit type is list of Nucmer_Match objects (sig_matches) + list of Nucmer_Match objects (fragments) that map to the sig_match objects
 
 #save progress
 f = open('pickled_sigmatches', 'wb')
 pickle.dump(collated_sig_matches, f)
 f.close()
 
+f = open('pickled_fragmatches', 'wb')
+pickle.dump(fragments, f)
+f.close()
+
 #cluster all significant matches & sort
-if cluster_method == 'single-linkage':
+if cluster_method == 'single-linkage': #now deprecated 
     clusters = single_linkage_cluster.cluster_nucmer_matches(collated_sig_matches)
+    
 elif cluster_method == 'union-find':
-    disjoint_set_array = {}
-    links = [m.seqs for m in collated_sig_matches]
-    for link in links:
-        union_find_cluster.union(link[0], link[1], disjoint_set_array)
-    union_find_cluster.cluster_conversion(disjoint_set_array)
-    clusters = union_find_cluster.extract_clusters(disjoint_set_array)
+    clusters = union_find_cluster.union_find_pipe(collated_sig_matches)
 
 single_linkage_cluster.sort_clusters(clusters)
 #exit type is list of Contig_Cluster objects
 
+#find fragments linked to clusters
+frag_seqs = [m.seqs.index(min(m.lengths)) for m in fragments]
+
+
+
+
 #run analysis on clusters
 for c in clusters:
-    assembly_node_dict = c.split_names()
-    c.retrieve_seqs(assembly_node_dict, assembly_dir = assembly_dir) #locations set at beginning of script
+    c.retrieve_seqs(assembly_dir = assembly_dir) #locations set at beginning of script. Does not return anything
     
 f = open('pickled_clusters', 'wb')
 pickle.dump(clusters, f)
